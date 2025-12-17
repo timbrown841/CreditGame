0491aeb7b2dadb55bede4b49894071d9164dc56d
timbrown841 <timbrown841@outlook.com>
2025-12-17 16:37:14 +0000

    Update server.js

--- server.js
+++ server.js
@@ -39,12 +39,11 @@ const transporter = nodemailer.createTransport({
   secure: false,                       // STARTTLS on 587
   requireTLS: true,
   auth: { user: process.env.SMTP_USER, pass: process.env.SMTP_PASS },
-  connectionTimeout: 15000,            // 15s
-  greetingTimeout: 10000,              // 10s
-  socketTimeout: 20000,                // 20s
-  pool: true,                          // optional but helps
-  maxConnections: 2,
-  maxMessages: 20
+  connectionTimeout: 20000,
+  greetingTimeout: 20000,
+  socketTimeout: 30000,
+	// Office 365 sometimes prefers IPv4 from some hosts
+  family: 4
 });
 
 transporter.verify()
